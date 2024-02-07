765132e9eceb76d4346e5464b1632c85616ebe42
just-mika <mikaelaamon@gmail.com>
2024-02-07 18:30:26 +0800

    Added Main Menu

--- MPFunctions.c
+++ MPFunctions.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
 #include "MPHeader.h"
 
 void displayTitle()
--- S15AAmonM.c
+++ S15AAmonM.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <windows.h>
 #include "MPHeader.h"
 
 int main()
 {
-	displayTitle();
+	int nSelect;
 	
+	do
+	{
+		displayTitle();
+		printf("[1] Manage Data\n");
+		printf("[2] Play\n");
+		printf("[3] Exit\n");
+		printf("------------------------------------------------------------------\n");
+		printf("Enter: ");
+		scanf("%d", &nSelect);
+		if(nSelect != 1 && nSelect != 2 && nSelect != 3)
+		{
+			printf("Invalid input!\n");
+			sleep(1);
+			system("cls");
+		}
+	} while(nSelect != 1 && nSelect != 2 && nSelect != 3);
 	
+	switch(nSelect)
+	{
+		case 1:
+			printf("MANAGE DATA STILL IN PROGRESS");
+			//ManageData()
+			break;
+		case 2:
+			//PlayGame()
+			printf("PLAY STILL IN PROGRESS");
+			break;
+		case 3:
+			printf("Thanks for playing! :D");
+			sleep(1);
+			break;
+	}
 	return 0;
 }
